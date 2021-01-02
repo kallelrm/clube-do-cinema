e4202015e433e09a497e8ef1e358f3377a355a95
dbrtvrsky <kallelr@outlook.com>
2021-01-02 14:54:07 -0300

    adding createdb script

---
+++ createdb.sql
@@ -0,0 +1,12 @@
+-- Database: clube_do_cinema
+
+-- DROP DATABASE clube_do_cinema;
+
+CREATE DATABASE clube_do_cinema
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
